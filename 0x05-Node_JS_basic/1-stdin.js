53c11365f7aa527f327cf96b7cf482833e64f56a
riley-kyule <phillipkyule@gmail.com>
2024-05-28 16:02:30 +0300

    Stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
+/* eslint-disable prefer-arrow-callback */
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  // Process input
+  const data = process.stdin.read();
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+  if (data) {
+    // Output result to stdout
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
 
+process.stdin.on('end', function display() {
+  // Do this on EOF from other commands
+  process.stdout.write('This important software is now closing\n');
+});
