72bdae6ecc5e7b128147510122e7da3f5b37433f
riley-kyule <phillipkyule@gmail.com>
2024-05-29 15:13:50 +0300

    Console + others

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,8 +3,8 @@
  * @param {String} arg is message to display
  */
 function displayMessage(arg) {
-  // print to STDOUT
-  console.log(arg);
-}
-
-module.exports = displayMessage;
+    // print to STDOUT
+    console.log(arg);
+  }
+  
+  module.exports = displayMessage;
